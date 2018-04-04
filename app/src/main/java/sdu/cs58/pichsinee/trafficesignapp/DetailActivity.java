6be89dd23ed874173bc277021e507a464b7459ee
Atsadawut030 <evilnot_123@hotmail.co.th>
2018-04-04 10:55:23 +0700

    Initial commit

---
+++ app/src/main/java/sdu/cs58/pichsinee/trafficesignapp/DetailActivity.java
@@ -0,0 +1,34 @@
+package sdu.cs58.pichsinee.trafficesignapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    TextView titleTextView , detailTextView;
+    ImageView signImageView ;
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //intial view ผู้ตัวแปรบน java กับ xml
+        titleTextView = findViewById(R.id.txvTitle);
+        detailTextView = findViewById(R.id.txvDetail);
+        signImageView = findViewById(R.id.imvSign);
+
+        //รับข้อมูลจากการ Intent ไปแสดงผลบนหน้าจอ XML
+        titleTextView.setText(getIntent().getStringExtra("Title"));
+        detailTextView.setText(getIntent().getStringExtra("Detail"));
+        signImageView.setImageResource(getIntent().getIntExtra("Image",R.drawable.traffic_01    ));
+    }//end onCreate Method
+
+    public void clickBack(View view) {
+        finish();
+    }//end clickBack Method
+}//end Class
